af553a65ed9053dd2be82e88f497852c3da0efce
aleksandarcrvcportfolio <aleksandar2cirovic@gmail.com>
2014-09-24 23:10:46 +0200

    Simple web api with angular app,

---
+++ Products.Web.Api.Test/ProductsControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Products.Web.Api.Controllers;
+
+namespace Products.Web.Api.Test
+{
+    [TestClass]
+    public class ProductsControllerTest
+    {
+        [TestMethod]
+        public void GetAllCustomers()
+        {
+            var prodController = new ProductsController();
+            var products = prodController.GetAllProducts();
+            Assert.IsNotNull(products);
+        }
+
+        [TestMethod]
+        public void Try_To_Get_Invalid_Product_Returns_Null()
+        {
+            var prodController = new ProductsController();
+            var product = prodController.GetProduct(-1);
+            Assert.IsNull(product);
+        }
+    }
+}
---
+++ Products.Web.Api.Tests/ProductControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Products.Web.Api.Controllers;
+
+namespace Products.Web.Api.Tests
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void GetAllCustomers()
+        {
+            var prodController = new ProductsController();
+            var products = prodController.GetAllProducts();
+            Assert.IsNotNull(products);
+        }
+
+        [TestMethod]
+        public void Try_To_Get_Invalid_Product_Returns_Null()
+        {
+            var prodController = new ProductsController();
+            var product = prodController.GetProduct(-1);
+            Assert.IsNull(product);
+        }
+    }
+}
