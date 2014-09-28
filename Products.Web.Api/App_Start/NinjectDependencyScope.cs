bb98a892b197074ef6f33df9568d8362ca00a555
aleksandarcrvcportfolio <aleksandar2cirovic@gmail.com>
2014-09-28 21:59:43 +0200

    Added moq and ninject to project for testing purposes.

---
+++ Products.Web.Api/App_Start/NinjectDependencyScope.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Dependencies;
+using Ninject;
+using Ninject.Syntax;
+using System.Web;
+using Ninject.Web.Common;
+
+namespace Products.Web.Api.App_Start
+{
+    public class NinjectDependencyScope : IDependencyScope
+    {
+        IResolutionRoot resolver;
+
+        public NinjectDependencyScope(IResolutionRoot resolver)
+        {
+            this.resolver = resolver;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (resolver == null)
+                throw new ObjectDisposedException("this", "This scope has been disposed");
+
+            return resolver.TryGet(serviceType);
+        }
+
+        public System.Collections.Generic.IEnumerable<object> GetServices(Type serviceType)
+        {
+            if (resolver == null)
+                throw new ObjectDisposedException("this", "This scope has been disposed");
+
+            return resolver.GetAll(serviceType);
+        }
+
+        public void Dispose()
+        {
+            IDisposable disposable = resolver as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+
+            resolver = null;
+        }
+    }
+
+    // This class is the resolver, but it is also the global scope
+    // so we derive from NinjectScope.
+    public class NinjectDependencyResolver : NinjectDependencyScope, IDependencyResolver
+    {
+        IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+            : base(kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectDependencyScope(kernel.BeginBlock());
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+            RegisterServices(kernel);
+
+            // Install our Ninject-based IDependencyResolver into the Web API config
+            GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(kernel);
+
+            return kernel;
+        }
+
+        private static void RegisterServices(IKernel kernel)
+        {
+            // This is where we tell Ninject how to resolve service requests
+            //kernel.Bind<IProductRepository>().To<ProductRepository>();
+        }
+    }
+}
